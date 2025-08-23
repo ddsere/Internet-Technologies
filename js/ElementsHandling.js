97b6883f75f30dffec132fd0a548d3e4714f878d
Daham <157113991+ddsere@users.noreply.github.com>
2025-08-23 10:00:28 +0530

    ElementsHandling done

--- js/ElementsHandling.js
+++ js/ElementsHandling.js
@@ -1,1 +1,28 @@
 console.log("ElementsHandling.js is loaded...");
+
+let newDiv = document.createElement("div");
+console.log(newDiv);
+
+newDiv.id = "alert-success";
+newDiv.style.backgroundColor = "green";
+newDiv.style.color = "white";
+newDiv.style.padding = "12px";
+newDiv.style.borderRadius = "8px";
+newDiv.textContent = "Success! Your action was completed.";
+
+const card = document.querySelector(".card");
+
+//Append() - Inserts the element into the specific element
+card.append(newDiv);
+
+const alert = document.getElementById("alert-success");
+// alert.remove();
+
+let newDiv2 = newDiv.cloneNode(true);
+// card.append(newDiv2);
+
+let clonedDiv = newDiv.cloneNode(true);
+clonedDiv.id = "alert-error";
+clonedDiv.style.backgroundColor = "red";
+clonedDiv.textContent = "Error! Something went wrong.";
+card.append(clonedDiv);
